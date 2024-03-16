6d26f6d75d6656f437a21e92f20e31ca4943a179
Im-KVA <130117095+Im-KVA@users.noreply.github.com>
2024-03-16 14:35:20 +0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,31 @@
 public class Main {
     public static void main(String[] args) {
         int[] numbers = {1, 3, 5, 6, 2, 7, 8, 0, 4, 3, 9, 2, 8, 1, 0, 5, 7, 4, 6, 9, 3, 2, 1, 8, 4, 0, 6, 7, 9, 5, 3, 2, 1, 0, 8, 4, 6, 7, 9, 5, 3, 2, 1, 0, 8, 4, 6, 7, 9, 5, 3, 2, 1, 0, 8, 4, 6, 7, 9, 5, 3, 2, 1, 0, 8, 4, 6, 7, 9, 5, 3, 2, 1, 0};
-    
 
+        // T1 tinh tong
+        Thread sumThread = new Thread(() -> {
+            int sum = 0;
+            for (int num : numbers) {
+                sum += num;
+            }
+            System.out.println("Tổng của các phần tử trong mảng là: " + sum);
+        });
 
-        Viết mã của bạn vào đây
-    
+        // T2 tim phan tu lon nhat
+        Thread maxThread = new Thread(() -> {
+            int max = Integer.MIN_VALUE;
+            for (int num : numbers) {
+                if (num > max) {
+                    max = num;
+                }
+            }
+            System.out.println("Phần tử lớn nhất trong mảng là: " + max);
+        });
+
+        // Khoi dong 2 luong
+        maxThread.start();
+        sumThread.start();
     }
 }
+
+
